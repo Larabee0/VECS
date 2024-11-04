1aa8615278912163d4bcba4a8e298fe50e9af5b1
William Vickers Hastings <wvh200@gmail.com>
2024-11-04 23:50:50 +0000

    Project Configuration

---
+++ SDL-Vulkan-CS/Program.cs
@@ -0,0 +1,13 @@
+﻿using SDL3;
+using Vortice.Vulkan;
+
+namespace SDL_Vulkan_CS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
