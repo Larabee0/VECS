6ff2f963eb5f36b8dabdbad4210ac2ec96f7b7e7
William Vickers Hastings <wvh200@gmail.com>
2024-11-13 15:29:22 +0000

    Neatened as much as posible and commented the RenderPipeline and ConfigInfo scripts

--- SDL-Vulkan-CS/VulkanBackend/RenderPipeline.cs
+++ SDL-Vulkan-CS/VulkanBackend/RenderPipeline.cs
@@ -14,13 +14,23 @@ namespace SDL_Vulkan_CS
         private VkShaderModule _vertShaderModule;
         private VkShaderModule _fragShaderModule;
 
-        public RenderPipeline(GraphicsDevice device, string vertFilePath, string fragFilePath, ref RenderPipelineConfigInfo configInfo)
+        public RenderPipeline(GraphicsDevice device, string vertFilePath, string fragFilePath, RenderPipelineConfigInfo configInfo)
         {
             _device = device;
-            CreateGraphicsPipeline(vertFilePath, fragFilePath, ref configInfo);
+            CreateGraphicsPipeline(vertFilePath, fragFilePath, configInfo);
         }
 
-        private unsafe void CreateGraphicsPipeline(string vertFilePath, string fragFilePath, ref RenderPipelineConfigInfo configInfo)
+        /// <summary>
+        /// Create a VkPipeline <see cref="_graphicsPipeline"/> with the given RenderPipelineConfigInfo and vertex and fragment shaders.
+        /// 
+        /// This is so long because stackalloc made down the call stack cannot trickle back up the call stack.
+        /// 
+        /// </summary>
+        /// <param name="vertFilePath"></param>
+        /// <param name="fragFilePath"></param>
+        /// <param name="configInfo"></param>
+        /// <exception cref="ArgumentException"></exception>
+        private unsafe void CreateGraphicsPipeline(string vertFilePath, string fragFilePath, RenderPipelineConfigInfo configInfo)
         {
             if (configInfo.pipelineLayout == VkPipelineLayout.Null)
             {
@@ -32,53 +42,46 @@ namespace SDL_Vulkan_CS
                 throw new ArgumentException("Cannot create graphics pipeline:: no renderPass layout provided in configInfo");
             }
 
-            VkPipelineShaderStageCreateInfo* shaderStages = stackalloc VkPipelineShaderStageCreateInfo[2];
-            GetShaderStageCreateInfo(vertFilePath, fragFilePath,shaderStages);
-
-            var bindingDescriptions = configInfo.BindingDescriptions;
-            var attributeDescriptions = configInfo.AttributeDescriptions;
-
-            VkVertexInputBindingDescription* pBindingDescriptions = stackalloc VkVertexInputBindingDescription[bindingDescriptions.Length];
-            VkVertexInputAttributeDescription* pAttributeDescriptions = stackalloc VkVertexInputAttributeDescription[attributeDescriptions.Length];
-            for (int i = 0; i < bindingDescriptions.Length; i++)
-            {
-                pBindingDescriptions[i] = bindingDescriptions[i];
-            }
-            for (int i = 0; i < attributeDescriptions.Length; i++)
-            {
-                pAttributeDescriptions[i] = attributeDescriptions[i];
-            }
-
-            VkPipelineVertexInputStateCreateInfo vertexInputInfo = new()
-            {
-                vertexAttributeDescriptionCount = (uint)attributeDescriptions.Length,
-                vertexBindingDescriptionCount = (uint)bindingDescriptions.Length,
-                pVertexAttributeDescriptions = pAttributeDescriptions,
-                pVertexBindingDescriptions = pBindingDescriptions
-            };
-
+            // Fix the properties needed for Graphics Pipeline Create Info
             var vkDynamicInfo = configInfo.dynamicInfo;
             var depthStencilInfo = configInfo.depthStencilInfo;
             var colourBlendInfo = configInfo.colourBlendInfo;
             var colourBlendAttachment = configInfo.colourBlendAttachment;
-            colourBlendInfo.pAttachments = &colourBlendAttachment;
             var inputAssemblyInfo = configInfo.inputAssemblyInfo;
             var viewportInfo = configInfo.viewportInfo;
             var multisampleInfo = configInfo.multisampleInfo;
             var rasterizationInfo = configInfo.rasterizationInfo;
-            //var rasterizationInfo = VkPipelineRasterizationStateCreateInfo.CullCounterClockwise;
 
-            VkDynamicState* pDynamicStats = stackalloc VkDynamicState[2]
+            // Assign remaining memory pointers
+            colourBlendInfo.pAttachments = &colourBlendAttachment;
+
+            VkDynamicState* pDynamicStates = stackalloc VkDynamicState[configInfo.dynamicStateEnables.Length];
+            
+            for (int i = 0; i < configInfo.dynamicStateEnables.Length; i++)
             {
-                VkDynamicState.Viewport, VkDynamicState.Scissor
-            };
-            vkDynamicInfo.pDynamicStates = pDynamicStats;
+                pDynamicStates[i] = configInfo.dynamicStateEnables[i];
+            }
+
+            vkDynamicInfo.pDynamicStates = pDynamicStates;
+
+            // Vertex Input State Create Info
+            VkVertexInputBindingDescription* pBindingDescriptions = stackalloc VkVertexInputBindingDescription[configInfo.BindingDescriptions.Length];
+            VkVertexInputAttributeDescription* pAttributeDescriptions = stackalloc VkVertexInputAttributeDescription[configInfo.AttributeDescriptions.Length];
+            VkPipelineVertexInputStateCreateInfo vertexInputState = GetVertexInputState(
+                configInfo.BindingDescriptions,
+                configInfo.AttributeDescriptions,
+                pBindingDescriptions,
+                pAttributeDescriptions);
+
+            // Shader stages
+            VkPipelineShaderStageCreateInfo* shaderStages = stackalloc VkPipelineShaderStageCreateInfo[2];
+            GetShaderStageCreateInfo(vertFilePath, fragFilePath, shaderStages);
 
             VkGraphicsPipelineCreateInfo pipelineInfo = new()
             {
                 stageCount = 2,
                 pStages = shaderStages,
-                pVertexInputState = &vertexInputInfo,
+                pVertexInputState = &vertexInputState,
                 pInputAssemblyState = &inputAssemblyInfo,
                 pViewportState = &viewportInfo,
                 pRasterizationState = &rasterizationInfo,
@@ -95,12 +98,53 @@ namespace SDL_Vulkan_CS
                 basePipelineHandle = VkPipeline.Null
             };
 
+
             if (Vulkan.vkCreateGraphicsPipeline(_device.Device, pipelineInfo, out _graphicsPipeline) != VkResult.Success)
             {
                 throw new Exception("Failed to create graphics pipeline!");
             }
         }
 
+        /// <summary>
+        /// Create aVertex Input State CreateInfo struct for the graphics pipeline given the input binding and attribute descriptions
+        /// </summary>
+        /// <param name="bindingDescriptions">vertex binding descriptions of the <see cref="Vertex.GetBindingDescriptions" struct/></param>
+        /// <param name="attributeDescriptions">vertex binding descriptions of the <see cref="Vertex.GetAttributeDescriptions" struct</param>
+        /// <param name="pBindingDescriptions">stack memory destination for vertex binding descriptions</param>
+        /// <param name="pAttributeDescriptions">stack memory destination for vertex attrubute descriptions</param>
+        /// <returns></returns>
+        private static unsafe VkPipelineVertexInputStateCreateInfo GetVertexInputState(VkVertexInputBindingDescription[] bindingDescriptions, VkVertexInputAttributeDescription[] attributeDescriptions, VkVertexInputBindingDescription* pBindingDescriptions, VkVertexInputAttributeDescription* pAttributeDescriptions)
+        {
+            for (int i = 0; i < bindingDescriptions.Length; i++)
+            {
+                pBindingDescriptions[i] = bindingDescriptions[i];
+            }
+            for (int i = 0; i < attributeDescriptions.Length; i++)
+            {
+                pAttributeDescriptions[i] = attributeDescriptions[i];
+            }
+
+            VkPipelineVertexInputStateCreateInfo vertexInputInfo = new()
+            {
+                vertexAttributeDescriptionCount = (uint)attributeDescriptions.Length,
+                vertexBindingDescriptionCount = (uint)bindingDescriptions.Length,
+                pVertexAttributeDescriptions = pAttributeDescriptions,
+                pVertexBindingDescriptions = pBindingDescriptions
+            };
+            return vertexInputInfo;
+        }
+
+        /// <summary>
+        /// Loads and creates VkShaderModules for the input vert and frag shader file paths,
+        /// then packages them into the pre-allocated VkPipelineShaderStageCreateInfo point for use
+        /// in creating a graphics pipeline.
+        /// 
+        /// The shader modules are cached in the Render Pipeline so they can be properly disposed when the pipeline is destroyed.
+        /// 
+        /// </summary>
+        /// <param name="vertFilePath">file path to compiled vertex shader</param>
+        /// <param name="fragFilePath">file path to compiled fragment shader</param>
+        /// <param name="shaderStages">stack allocated Pipeline Shader Stage Info</param>
         private unsafe void GetShaderStageCreateInfo(string vertFilePath, string fragFilePath, VkPipelineShaderStageCreateInfo* shaderStages)
         {
             Vulkan.vkCreateShaderModule(_device.Device, File.ReadAllBytes(vertFilePath), null, out _vertShaderModule);
@@ -129,7 +173,6 @@ namespace SDL_Vulkan_CS
                 pNext = null,
                 pSpecializationInfo = null
             };
-            //return shaderStages;
         }
 
         public void Bind(VkCommandBuffer commandBuffer)
@@ -142,146 +185,6 @@ namespace SDL_Vulkan_CS
             Vulkan.vkDestroyShaderModule(_device.Device, _vertShaderModule);
             Vulkan.vkDestroyShaderModule(_device.Device, _fragShaderModule);
             Vulkan.vkDestroyPipeline(_device.Device,_graphicsPipeline);
-        }
-
-        public static unsafe void DefaultPipelineConfigInfo(ref RenderPipelineConfigInfo configInfo)
-        {
-            // input assembly info
-            var inputAssemblyInfo = configInfo.inputAssemblyInfo;
-
-            inputAssemblyInfo.sType = VkStructureType.PipelineInputAssemblyStateCreateInfo;
-            inputAssemblyInfo.topology = VkPrimitiveTopology.TriangleList;
-            inputAssemblyInfo.primitiveRestartEnable = false;
-
-            configInfo.inputAssemblyInfo = inputAssemblyInfo;
-
-            // viewport info
-            var viewportInfo = configInfo.viewportInfo;
-
-            viewportInfo.sType = VkStructureType.PipelineViewportStateCreateInfo;
-            viewportInfo.viewportCount = 1;
-            viewportInfo.pViewports = null;
-            viewportInfo.scissorCount = 1;
-            viewportInfo.pScissors = null;
-
-            configInfo.viewportInfo = viewportInfo;
-
-            // rasterization info
-            var rasterizationInfo = configInfo.rasterizationInfo;
-
-            rasterizationInfo.sType = VkStructureType.PipelineRasterizationStateCreateInfo;
-            rasterizationInfo.depthClampEnable = false;
-            rasterizationInfo.rasterizerDiscardEnable = false;
-            rasterizationInfo.polygonMode = VkPolygonMode.Fill;
-            rasterizationInfo.lineWidth = 1;
-            rasterizationInfo.cullMode = VkCullModeFlags.None;
-            rasterizationInfo.frontFace = VkFrontFace.Clockwise;
-            rasterizationInfo.depthBiasEnable = false;
-            rasterizationInfo.depthBiasConstantFactor = 0;
-            rasterizationInfo.depthBiasClamp = 0;
-            rasterizationInfo.depthBiasSlopeFactor = 0;
-
-            configInfo.rasterizationInfo = rasterizationInfo;
-
-            // multi sample info
-            var multisampleInfo = configInfo.multisampleInfo;
-
-            multisampleInfo.sType = VkStructureType.PipelineMultisampleStateCreateInfo;
-            multisampleInfo.sampleShadingEnable = false;
-            multisampleInfo.rasterizationSamples = VkSampleCountFlags.Count1;
-            multisampleInfo.minSampleShading = 1;
-            multisampleInfo.pSampleMask = null;
-            multisampleInfo.alphaToCoverageEnable = false;
-            multisampleInfo.alphaToOneEnable = false;
-
-            configInfo.multisampleInfo = multisampleInfo;
-
-            // colour blend attachment
-            var colourBlendAttachment = configInfo.colourBlendAttachment;
-
-            colourBlendAttachment.colorWriteMask = VkColorComponentFlags.All;
-            colourBlendAttachment.blendEnable = false;
-            colourBlendAttachment.srcColorBlendFactor = VkBlendFactor.One;
-            colourBlendAttachment.dstColorBlendFactor = VkBlendFactor.Zero;
-            colourBlendAttachment.colorBlendOp = VkBlendOp.Add;
-            colourBlendAttachment.srcAlphaBlendFactor = VkBlendFactor.One;
-            colourBlendAttachment.dstAlphaBlendFactor = VkBlendFactor.Zero;
-            colourBlendAttachment.alphaBlendOp = VkBlendOp.Add;
-
-            configInfo.colourBlendAttachment = colourBlendAttachment;
-
-            // colour blend info
-            var colourBlendInfo = configInfo.colourBlendInfo;
-
-            colourBlendInfo.sType = VkStructureType.PipelineColorBlendStateCreateInfo;
-            colourBlendInfo.logicOpEnable = false;
-            colourBlendInfo.logicOp = VkLogicOp.Copy;
-            colourBlendInfo.attachmentCount = 1;
-
-            
-            colourBlendInfo.pAttachments = &colourBlendAttachment;
-
-            colourBlendInfo.blendConstants[0] = 0;
-            colourBlendInfo.blendConstants[1] = 0;
-            colourBlendInfo.blendConstants[2] = 0;
-            colourBlendInfo.blendConstants[3] = 0;
-
-            configInfo.colourBlendInfo = colourBlendInfo;
-
-            // depth stencil info
-            var depthStencilInfo = configInfo.depthStencilInfo;
-
-            depthStencilInfo.sType = VkStructureType.PipelineDepthStencilStateCreateInfo;
-            depthStencilInfo.depthTestEnable = true;
-            depthStencilInfo.depthWriteEnable = true;
-            depthStencilInfo.depthCompareOp = VkCompareOp.Less;
-            depthStencilInfo.depthBoundsTestEnable = false;
-            depthStencilInfo.minDepthBounds = 0.0f;
-            depthStencilInfo.maxDepthBounds = 1.0f;
-            depthStencilInfo.stencilTestEnable = false;
-            depthStencilInfo.front = default;
-            depthStencilInfo.back = default;
-
-            configInfo.depthStencilInfo = depthStencilInfo;
-
-            // dynamic Info
-
-            configInfo.dynamicStateEnables = [VkDynamicState.Viewport, VkDynamicState.Scissor];
-
-
-            configInfo.dynamicInfo.sType = VkStructureType.PipelineDynamicStateCreateInfo;
-
-
-            //for (int i = 0; i < configInfo.dynamicStateEnables.Length; i++)
-            //{
-            //    pDynamicStats[i] = configInfo.dynamicStateEnables[i];
-            //}
-
-            configInfo.dynamicInfo.dynamicStateCount = (uint)configInfo.dynamicStateEnables.Length;
-            configInfo.dynamicInfo.flags = 0;
-
-
-            // vertex descriptors
-            configInfo.BindingDescriptions = Vertex.GetBindingDescriptions();
-            configInfo.AttributeDescriptions = Vertex.GetAttributeDescriptions();
-            //return configInfo;
-        }
-
-        public static void EnableAlphaBlending(ref RenderPipelineConfigInfo configInfo)
-        {
-            var colourBlendAttachment = configInfo.colourBlendAttachment;
-
-            colourBlendAttachment.blendEnable = true;
-            colourBlendAttachment.colorWriteMask = VkColorComponentFlags.All;
-
-            colourBlendAttachment.srcColorBlendFactor = VkBlendFactor.SrcAlpha;
-            colourBlendAttachment.dstColorBlendFactor = VkBlendFactor.OneMinusSrcAlpha;
-            colourBlendAttachment.colorBlendOp = VkBlendOp.Add;
-            colourBlendAttachment.srcAlphaBlendFactor = VkBlendFactor.One;
-            colourBlendAttachment.dstAlphaBlendFactor = VkBlendFactor.Zero;
-            colourBlendAttachment.alphaBlendOp = VkBlendOp.Add;
-
-            configInfo.colourBlendAttachment = colourBlendAttachment;
         }
     }
 }
--- SDL-Vulkan-CS/VulkanBackend/RenderPipelineConfigInfo.cs
+++ SDL-Vulkan-CS/VulkanBackend/RenderPipelineConfigInfo.cs
@@ -2,6 +2,11 @@
 
 namespace SDL_Vulkan_CS
 {
+    /// <summary>
+    /// Managed none-pointer struct used in the creation and configuration of a <see cref="VkGraphicsPipelineCreateInfo"/>
+    /// 
+    /// A whole load of configuration data for a a Vk Graphics Pipeline.
+    /// </summary>
     public struct RenderPipelineConfigInfo
     {
         public VkVertexInputBindingDescription[] BindingDescriptions;
@@ -18,5 +23,139 @@ namespace SDL_Vulkan_CS
         public VkPipelineLayout pipelineLayout;
         public VkRenderPass renderPass;
         public uint subpass;
+
+        /// <summary>
+        /// Default graphics pipeline configuration. Because of course vulkan doesn't have a default.
+        /// </summary>
+        /// <returns></returns>
+        public static unsafe RenderPipelineConfigInfo DefaultPipelineConfigInfo()
+        {
+
+            VkPipelineColorBlendStateCreateInfo colourBlendInfo = new()
+            {
+                logicOpEnable = false,
+                logicOp = VkLogicOp.Copy,
+                attachmentCount = 1
+            };
+            colourBlendInfo.blendConstants[0] = 0;
+            colourBlendInfo.blendConstants[1] = 0;
+            colourBlendInfo.blendConstants[2] = 0;
+            colourBlendInfo.blendConstants[3] = 0;
+
+            VkDynamicState[] dynamicStateEnables = [VkDynamicState.Viewport, VkDynamicState.Scissor];
+            VkPipelineDynamicStateCreateInfo dynamicInfo = new()
+            {
+                dynamicStateCount = (uint)dynamicStateEnables.Length,
+                flags = 0
+            };
+
+            return new()
+            {
+                inputAssemblyInfo = new()
+                {
+                    topology = VkPrimitiveTopology.TriangleList,
+                    primitiveRestartEnable = false
+                },
+
+                viewportInfo = new()
+                {
+                    viewportCount = 1,
+                    pViewports = null,
+                    scissorCount = 1,
+                    pScissors = null
+                },
+
+                rasterizationInfo = new()
+                {
+                    depthClampEnable = false,
+                    rasterizerDiscardEnable = false,
+                    polygonMode = VkPolygonMode.Fill,
+                    lineWidth = 1,
+                    cullMode = VkCullModeFlags.None,
+                    frontFace = VkFrontFace.Clockwise,
+                    depthBiasEnable = false,
+                    depthBiasConstantFactor = 0,
+                    depthBiasClamp = 0,
+                    depthBiasSlopeFactor = 0
+                },
+
+                multisampleInfo = new()
+                {
+                    sampleShadingEnable = false,
+                    rasterizationSamples = VkSampleCountFlags.Count1,
+                    minSampleShading = 1,
+                    pSampleMask = null,
+                    alphaToCoverageEnable = false,
+                    alphaToOneEnable = false
+                },
+
+                colourBlendInfo = colourBlendInfo,
+
+                colourBlendAttachment = new()
+                {
+                    colorWriteMask = VkColorComponentFlags.All,
+                    blendEnable = false,
+                    srcColorBlendFactor = VkBlendFactor.One,
+                    dstColorBlendFactor = VkBlendFactor.Zero,
+                    colorBlendOp = VkBlendOp.Add,
+                    srcAlphaBlendFactor = VkBlendFactor.One,
+                    dstAlphaBlendFactor = VkBlendFactor.Zero,
+                    alphaBlendOp = VkBlendOp.Add
+                },
+
+                depthStencilInfo = new()
+                {
+                    sType = VkStructureType.PipelineDepthStencilStateCreateInfo,
+                    depthTestEnable = true,
+                    depthWriteEnable = true,
+                    depthCompareOp = VkCompareOp.Less,
+                    depthBoundsTestEnable = false,
+                    minDepthBounds = 0.0f,
+                    maxDepthBounds = 1.0f,
+                    stencilTestEnable = false,
+                    front = default,
+                    back = default
+                },
+
+                dynamicStateEnables = dynamicStateEnables,
+
+                dynamicInfo = dynamicInfo,
+
+                BindingDescriptions = Vertex.GetBindingDescriptions(),
+                AttributeDescriptions = Vertex.GetAttributeDescriptions()
+            };
+        }
+
+        public static RenderPipelineConfigInfo DefaultPipelineConfigInfo(VkRenderPass renderPass, VkPipelineLayout pipelineLayout)
+        {
+            var pipelineConfigInfo = DefaultPipelineConfigInfo();
+
+            pipelineConfigInfo.renderPass = renderPass;
+            pipelineConfigInfo.pipelineLayout = pipelineLayout;
+
+            return pipelineConfigInfo;
+        }
+
+        /// <summary>
+        /// Modify the given configInfo to enable alpha blending of the colour channel.
+        /// </summary>
+        /// <param name="configInfo">Configuration to modify</param>
+        public static void EnableAlphaBlending(ref RenderPipelineConfigInfo configInfo)
+        {
+            var colourBlendAttachment = configInfo.colourBlendAttachment;
+
+            colourBlendAttachment.blendEnable = true;
+            colourBlendAttachment.colorWriteMask = VkColorComponentFlags.All;
+
+            colourBlendAttachment.srcColorBlendFactor = VkBlendFactor.SrcAlpha;
+            colourBlendAttachment.dstColorBlendFactor = VkBlendFactor.OneMinusSrcAlpha;
+            colourBlendAttachment.colorBlendOp = VkBlendOp.Add;
+            colourBlendAttachment.srcAlphaBlendFactor = VkBlendFactor.One;
+            colourBlendAttachment.dstAlphaBlendFactor = VkBlendFactor.Zero;
+            colourBlendAttachment.alphaBlendOp = VkBlendOp.Add;
+
+            configInfo.colourBlendAttachment = colourBlendAttachment;
+        }
+
     }
 }
