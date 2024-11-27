5217a9a08e47f9950b5b7749b78a56879473318b
William Vickers Hastings <wvh200@gmail.com>
2024-11-27 20:34:07 +0000

    Added terrain generation

---
+++ SDL-Vulkan-CS/Artifact/Generator/NoiseSettings.cs
@@ -0,0 +1,41 @@
+﻿using System.Numerics;
+
+namespace SDL_Vulkan_CS.Artifact.Generator
+{
+    public enum FilterType { Simple, Rigid };
+
+    public class SimpleNoiseSettings
+    {
+        public FilterType filterType;
+
+        public float strength = 1;
+        public int numLayers = 1;
+        public float baseRoughness = 1;
+        public float roughness = 2;
+        public float persistence = 0.5f;
+        public Vector3 centre;
+        public float offset = 0;
+
+        public float minValue = 0;
+        public bool gradientWeight = true;
+        public float gradientWeightMul = 0.1f;
+
+        public bool enabled = true;
+        public bool useFirstlayerAsMask = false;
+
+        public virtual float Evaluate(Vector3 point)
+        {
+            return SimpleNosieFilter.Evaluate(this,point);
+        }
+    }
+
+    public class RigidNoiseSettings : SimpleNoiseSettings
+    {
+        public float weightMultiplier = 0.8f;
+
+        public override float Evaluate(Vector3 point)
+        {
+            return RigidNoiseFilter.Evaluate(this,point);
+        }
+    }
+}
---
+++ SDL-Vulkan-CS/Artifact/Generator/ShapeGenerator.cs
@@ -0,0 +1,66 @@
+﻿using SDL_Vulkan_CS.VulkanBackend;
+using System;
+using System.Numerics;
+
+namespace SDL_Vulkan_CS.Artifact.Generator
+{
+    public class ShapeGenerator
+    {
+        public float _planetRadius = 1;
+        public int _seed = 0;
+        public bool _randomSeed = false;
+
+        public SimpleNoiseSettings[] _noiseFilters;
+
+        public void RandomiseSettings()
+        {
+            _seed = _randomSeed ? Random.Shared.Next(int.MinValue, int.MaxValue) : 0;
+
+            Random random = new(_seed);
+
+
+            for (int i = 0; i < _noiseFilters.Length; i++)
+            {
+                _noiseFilters[i].centre = new Vector3(random.Next(-1000, 1000), random.Next(-1000, 1000), random.Next(-1000, 1000));
+            }
+        }
+
+        public void RaiseMesh(Mesh mesh)
+        {
+            Vertex[] vertices = mesh.vertices;
+            for (int i = 0; i < vertices.Length; i++)
+            {
+                vertices[i].Position = CalculatePointOnPlanet(vertices[i].Position, out _);
+            }
+            mesh.vertices = vertices;
+            mesh.RecalculateNormals();
+        }
+
+        public Vector3 CalculatePointOnPlanet(Vector3 pointOnUnitSphere, out float elevation)
+        {
+            float firstLayerValue = 0f;
+            elevation = 0;
+
+            if (_noiseFilters.Length > 0)
+            {
+                firstLayerValue = _noiseFilters[0].Evaluate(pointOnUnitSphere);
+                if (_noiseFilters[0].enabled)
+                {
+                    elevation = firstLayerValue;
+                }
+            }
+
+            for (int i = 1; i < _noiseFilters.Length; i++)
+            {
+                if (_noiseFilters[i].enabled)
+                {
+                    float mask = _noiseFilters[i].useFirstlayerAsMask ? firstLayerValue : 1;
+                    elevation += _noiseFilters[i].Evaluate(pointOnUnitSphere) * mask;
+                }
+            }
+            elevation = _planetRadius * (1 + elevation);
+            // elevationMinMax.AddValue(elevation);
+            return pointOnUnitSphere * elevation;
+        }
+    }
+}
